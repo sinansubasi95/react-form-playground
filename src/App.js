2da7d83be73d6788ea97a2a9a3481fe46445109a
sinansubasi <sinan95@gmail.com>
2021-02-07 16:11:04 +0300

    login form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import FormikContainer from './components/FormikContainer';
+import LoginForm from './components/LoginForm';
 // import YoutubeForm from './components/YoutubeForm';
 
 function App() {
   return (
     <div className="App">
-      <FormikContainer />
+      <LoginForm />
+      {/* <FormikContainer /> */}
       {/* <YoutubeForm /> */}
     </div>
   );
