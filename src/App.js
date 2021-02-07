e8676c06bb0668f400245bba01dc56ae98aee572
sinansubasi <sinan95@gmail.com>
2021-02-07 12:23:24 +0300

    building reusable formik controls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import FormikContainer from './components/FormikContainer';
 import YoutubeForm from './components/YoutubeForm';
 
 function App() {
   return (
     <div className="App">
-      <YoutubeForm />
+      <FormikContainer />
+      {/* <YoutubeForm /> */}
     </div>
   );
 }
