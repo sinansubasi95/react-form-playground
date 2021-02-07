2f9b477b1270851a3ca498190c651a918523cdca
sinansubasi <sinan95@gmail.com>
2021-02-07 13:22:15 +0300

    select

--- src/components/FormikContainer.jsx
+++ src/components/FormikContainer.jsx
@@ -4,13 +4,21 @@ import * as Yup from 'yup';
 import FormikControl from './FormikControl';
 
 const FormikContainer = () => {
+    const dropdownOptions = [
+        { key: 'Select an option', value: ''},
+        { key: 'Option 1', value: 'option1'},
+        { key: 'Option 2', value: 'option2'},
+        { key: 'Option 3', value: 'option3'}
+    ];
     const initialValues = {
         email: '',
-        description: ''
+        description: '',
+        selectOption: ''
     }
     const validationSchema = Yup.object({
         email: Yup.string().required('Required'),
-        description: Yup.string().required('Required')
+        description: Yup.string().required('Required'),
+        selectOption: Yup.string().required('Required')
     });
     const onSubmit = values => console.log('Form data', values);
 
@@ -32,6 +40,12 @@ const FormikContainer = () => {
                         control="textarea"
                         label="Description"
                         name="description"
+                    />
+                    <FormikControl
+                        control="select"
+                        label="Select a topic"
+                        name="selectOption"
+                        options={dropdownOptions}
                     />
                     <button type='submit'>Submit</button>
                 </Form>
--- src/components/FormikControl.jsx
+++ src/components/FormikControl.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Input from './Input';
+import Select from './Select';
 import Textarea from './Textarea';
 
 const FormikControl = (props) => {
@@ -10,6 +11,7 @@ const FormikControl = (props) => {
         case 'textarea':
             return <Textarea {...rest} />
         case 'select':
+            return <Select {...rest} />
         case 'radio':
         case 'checkbox':
         case 'date':
