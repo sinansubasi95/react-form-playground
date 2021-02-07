97855ac39637069beb29190a8ebb5d98239e590f
sinansubasi <sinan95@gmail.com>
2021-02-07 16:31:20 +0300

    course enrollment form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import EnrollmentForm from './components/EnrollmentForm';
 import FormikContainer from './components/FormikContainer';
 import LoginForm from './components/LoginForm';
 import RegistrationForm from './components/RegistrationForm';
@@ -8,7 +9,8 @@ import RegistrationForm from './components/RegistrationForm';
 function App() {
   return (
     <div className="App">
-      <RegistrationForm />
+      <EnrollmentForm />
+      {/* <RegistrationForm /> */}
       {/* <LoginForm /> */}
       {/* <FormikContainer /> */}
       {/* <YoutubeForm /> */}
