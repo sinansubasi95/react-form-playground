df8c1ac68c56ea88f8f9b7d88ed6419f5f3b9c1a
sinansubasi <sinan95@gmail.com>
2021-02-07 16:26:20 +0300

    registration form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import FormikContainer from './components/FormikContainer';
 import LoginForm from './components/LoginForm';
+import RegistrationForm from './components/RegistrationForm';
 // import YoutubeForm from './components/YoutubeForm';
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
+      <RegistrationForm />
+      {/* <LoginForm /> */}
       {/* <FormikContainer /> */}
       {/* <YoutubeForm /> */}
     </div>
